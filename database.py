f3489b06d1e5e1b6c6ad15040098a2d306782486
anasazamov <anasazamov55@gmail.com>
2023-04-08 18:06:58 +0500

    Query propity method for any function

--- database.py
+++ database.py
@@ -14,38 +14,38 @@ def get_by_field(field: str, value: str):
 
 def get_user_by_id(user_id):
     '''Returns a user by id'''
-    pass
+    return users_table.search(User.doc_id==user_id)
 
 def get_user_by_email(email):
     '''Returns a user by email'''
-    pass
+    return users_table.search(User.email==email)
 
 
 def get_user_by_first_name(first_name):
     '''Returns a user by first name'''
-    pass
+    return users_table.search(User.first_name==first_name)
 
 def get_user_by_last_name(last_name):
     '''Returns a user by last name'''
-    pass
+    return users_table.search(User.last_name==last_name)
 
 def get_user_by_country(country):
     '''Returns a user by country'''
-    pass
+    return users_table.search(User.country==country)
 
 def get_users_full_name(user):
     '''Returns a user's full name'''
-    pass
+    return users_table.search(User.user==user)
 
 
 def update_user(user_id, field, value):
     '''Updates a user's field'''
-    pass
+    return users_table.search((User.doc_id==user_id) & (User[field].search(value)))
 
 def delete_user(user_id):
     '''Deletes a user'''
-    pass
+    users_table.remove(delete(user_id))
 
 def add_user(user):
     '''Adds a user to the database'''
-    pass
+    users_table.insert(user)
